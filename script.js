a5230d9b1a4a088082ce114241fe4030ca05293d
Santiago Cremon <santiago0988@hotmail.com>
2022-10-24 23:02:11 -0300

    Declare grid generator function

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ const screenContainer = document.querySelector(".screenContainer");
 // weell... will need a nice function to fill the screen with a customizable grid
 
 function createGrid(gridSize) {
-    for (let i = gridSize**2; i > 0; i--) {
-        console.log(i)
-        screenCell = document.createElement("div");
-        screenCell.style.height = "100%";
-        screenCell.style.flex = "1"
-        screenCell.style.border = "0.25px solid black";
-        
-        screenContainer.appendChild(screenCell);
+    
+    for (let i = gridSize; i > 0; i--) {
+        console.log("a")
+        column = generateColumn(gridSize);
+        screenContainer.appendChild(column);
     };
 };
 
-createGrid(8)
+function generateColumn(rowNumber) {
+
+    const gridColumn = document.createElement("div")
+    gridColumn.style.display = "flex";
+    gridColumn.style.flex = "1"
+    gridColumn.style.flexDirection = "column";
+    gridColumn.style.height = "100%";
+
+    for (let i = rowNumber; i > 0; i--) {
+        console.log("b");
+        const screenCell = document.createElement("div");
+        screenCell.style.border = "0.25px solid black";
+        screenCell.style.width = "100%";
+        screenCell.style.flex = "1 1 auto";
+        gridColumn.appendChild(screenCell)
+    };
+
+    return gridColumn
+};
+
+createGrid(50)
